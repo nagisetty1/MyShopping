826a2d18af074b722bd0ea6fe1fb956d243b78f9
NagiSetty.Akuthota <NagiSetty.Akuthota@ivycomptech.com>
2021-09-08 23:35:46 +0530

    added models project

---
+++ MyShopping.Models/Item.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShopping.Models
+{
+    public class Item
+    {
+        public string Id { get; set; }
+        public string Text { get; set; }
+        public string ImageSource { get; set; }
+        public string Description { get; set; }
+    }
+}
